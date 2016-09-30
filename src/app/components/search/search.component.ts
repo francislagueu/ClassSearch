02360cbfc8c21c416334a4e8df6ae729ff026558
Francis <lagson6@yahoo.fr>
2016-09-29 20:35:55 -0700

    format the navbar

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -7,7 +7,7 @@ import {FormGroup, FormControl} from '@angular/forms';
 // Decorator:
 @Component({
   // Attributes:
-  selector: 'search', //CSS selector for a host HTML element.  
+  selector: 'search', //CSS selector for a host HTML element.
   templateUrl: './search.component.html', // Specifies HTML to be inserted into the DOM when the component's view is rendered.
   styleUrls: ['./search.component.css'],
   providers: [SearchService]
@@ -17,6 +17,8 @@ export class SearchComponent{
   searchStr:string;
   term:any;
   searchRes:Class[];
+  obj:any[];
+
 
   terms = ['Fall-2016', 'Summer-2016', 'Spring-2016', 'Fall-2015', 'Summer-2015', 'Spring-2015', 'Fall-2014', 'Summer-2014', 'Spring-2014', 'Fall-2013', 'Summer-2013', 'Spring-2013'];
   model = new Class(this.terms[0]);
@@ -25,7 +27,7 @@ export class SearchComponent{
   //instructor;
 
   constructor(private _searchService: SearchService){
-    //this.instructor = SearchService.searchInstructor();
+
   }
 
   ngOnInit(){
@@ -37,17 +39,23 @@ export class SearchComponent{
   searchClass(){
     this._searchService.searchClass(this.searchStr, this.model.term)
         .subscribe(res=>{
-          this.searchRes = res.classes;
-          console.log(this.searchRes);
-        })
+          this.obj = this.searchRes = res.classes;
+
+
+
+          for(var i = 0; i< this.searchRes.length; i++){
+            console.log(this.searchRes[i]);
+          }
+        });
+        //this.printClass(this.searchClass);
+
   }
 
-/*  searchInstructor(){
-    this._searchService.searchInstructor(this.searchStr)
-      .subscribe(res=>{
-      this.searchRes = res.instructors;
-      console.log(this.searchRes);
-    })
-  }
-  */
+  // printClass(this.searchRes){
+  //     console.log('classes');
+  //   for(var i = 0; i< this.searchRes.length; i++){
+  //     if(searchRes[i].catalog_number === '100')
+  //     console.log(this.searchRes[i]);
+  //   }
+  // }
 }
