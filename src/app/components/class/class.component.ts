77382805f33812dc47f3161b6a7dcb336fdda8e0
francislagueu <lagson6@yahoo.fr>
2016-10-01 21:02:39 -0700

    Updated some files

--- src/app/components/class/class.component.ts
+++ src/app/components/class/class.component.ts
@@ -2,30 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import {SearchService} from '../../services/search.service';
 import {Class} from '../../../Class';
 import {ActivatedRoute} from '@angular/router';
+import {SearchComponent} from '../search/search.component';
 
 @Component({
   selector: 'class',
   templateUrl: './class.component.html',
   styleUrls: ['./class.component.css'],
+  directives:[SearchComponent],
   providers:[SearchService]
 })
 export class ClassComponent{
   id:string;
   term:string;
-  classes:any;
+  classes:ClassObj;
 
   constructor(private _searchService: SearchService,
               private _route: ActivatedRoute){
 }
 
   ngOnInit(){
-    this._route.params
+    return this._route.params
       .map(params=>params['course_id'])
       .subscribe((id)=>{
-        this._searchService.getClass(id)
-            .subscribe(classes=>{
-              this.classes = classes;
-              console.log(this.classes);
+        for(var i=0; i< classObj.length; i++){
+          if(classObj[i].course_id === id){
+            return classObj[i];
+          }
+          console.log(classObj[i]);
+        }
+      //  this._searchService.getClass(id)
+            //.subscribe(classes=>{
+              //this.classes = classes;
+
             })
       })
   }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {SearchService} from '../../services/search.service';
-import {Class} from '../../../CLass';
+import {Class} from '../../../Class';
+import {Classes} from '../../../Classes';
 import {ActivatedRoute} from '@angular/router';
 import {FormGroup, FormControl} from '@angular/forms';
+import {FilterComponent} from '../filter/filter.component';
 
 // Decorator:
 @Component({
   // Attributes:
-  selector: 'search', //CSS selector for a host HTML element.  
+  selector: 'search', //CSS selector for a host HTML element.
   templateUrl: './search.component.html', // Specifies HTML to be inserted into the DOM when the component's view is rendered.
   styleUrls: ['./search.component.css'],
   providers: [SearchService]
@@ -17,6 +19,9 @@ export class SearchComponent{
   searchStr:string;
   term:any;
   searchRes:Class[];
+  classObj = [];
+
+
 
   terms = ['Fall-2016', 'Summer-2016', 'Spring-2016', 'Fall-2015', 'Summer-2015', 'Spring-2015', 'Fall-2014', 'Summer-2014', 'Spring-2014', 'Fall-2013', 'Summer-2013', 'Spring-2013'];
   model = new Class(this.terms[0]);
@@ -25,7 +30,7 @@ export class SearchComponent{
   //instructor;
 
   constructor(private _searchService: SearchService){
-    //this.instructor = SearchService.searchInstructor();
+
   }
 
   ngOnInit(){
@@ -38,16 +43,24 @@ export class SearchComponent{
     this._searchService.searchClass(this.searchStr, this.model.term)
         .subscribe(res=>{
           this.searchRes = res.classes;
-          console.log(this.searchRes);
-        })
+
+
+
+          for(var i = 0; i< this.searchRes.length; i++){
+            console.log(this.searchRes[i]);
+            let obj = this.searchRes[i];
+            this.classObj.push(obj);
+            console.log(this.classObj[i]);
+          }
+        });
+        //this.printClass(this.searchClass);
+
   }
 
-/*  searchInstructor(){
-    this._searchService.searchInstructor(this.searchStr)
-      .subscribe(res=>{
-      this.searchRes = res.instructors;
-      console.log(this.searchRes);
-    })
-  }
-  */
-}
+  // printClass(this.searchRes){
+  //     console.log('classes');
+  //   for(var i = 0; i< this.searchRes.length; i++){
+  //     if(searchRes[i].catalog_number === '100')
+  //     console.log(this.searchRes[i]);
+  //   }
+  /
